23c577b1af5b943b4dd1383655b23186f750fb50
nocolour <nocolour@gmail.com>
2021-07-13 16:37:45 +0800

    Update PyQt5_HelloWorld.py

--- PyQt5_HelloWorld.py
+++ PyQt5_HelloWorld.py
@@ -1,20 +1,22 @@
 import sys
-# from PyQt5.QtCore import *
-# from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
-
 
 def window():
     app = QApplication(sys.argv)
     w = QWidget()
     b = QLabel(w)
-    b.setText("Hello World!")
+    f = QFont()
+    f.setFamily("Arial")
+    f.setPointSize(16)
+    b.setText("Hello World")
     w.setGeometry(100, 100, 200, 50)
+    b.setFont(f)
     b.move(50, 20)
-    w.setWindowTitle("PyQt5")
+    w.setWindowTitle("PyQt5 Tutorial")
     w.show()
     sys.exit(app.exec_())
-
-
+    
 if __name__ == '__main__':
     window()
